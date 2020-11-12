e44de90d52a527d3633eae3aba64804fac1e7241
Lukas Kairys <lukaskairys@gmail.com>
2020-11-12 13:03:07 +0200

    add range in copyright year

--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "./MainLayout.scss";
 import { ReactComponent as NotificationBell } from "assets/icons/notification-bell.svg";
 
+const creationYear = "(dark ages)";
 const currentYear = new Date().getFullYear(); //getting current year
+let year;
+if (creationYear === currentYear) {
+  year = creationYear;
+} else {
+  year = `${creationYear} - ${currentYear}`;
+}
 
-const MainLayout = () => {
+const MainLayout = ({ children }) => {
   return (
     <div className="main-layout">
       <header className="main-layout__header">
@@ -15,13 +23,18 @@ const MainLayout = () => {
           <div className="main-layout__profile"></div>
         </div>
       </header>
-      <main className="main-layout__main"></main>
+      <main className="main-layout__main">{children}</main>
       <footer className="main-layout__footer">
         <p className="main-layout__copyright">
-          copyright &copy; {currentYear} devbridge
+          copyright &copy; {year} devbridge
         </p>
       </footer>
     </div>
   );
 };
+
+MainLayout.propTypes = {
+  children: PropTypes.object,
+};
+
 export default MainLayout;
