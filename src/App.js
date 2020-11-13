dd0a7fc2b9206376c178b50a4f5c4e1bfab236da
aAntak <andriusk1997@gmail.com>
2020-11-13 12:22:29 +0200

    Add component semantics, style, and assets

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Registration from "../src/features/registration/components/Registration";
 import HelloWidget from "features/helloWidget/components/HelloWidget";
+import EatOutSection from "features/eatOutSection/components/eatOutSection";
 import ReservationSection from "features/reservationsSection/components/ReservationsSection";
 
 function App() {
   return (
-    <Router>
-      <>
-        {/* A <Switch> looks through its <Route>s and
-            renders the first one that matches the path. */}
-        {/* If you want to render something everywhere (in each page), render it outside the <Switch>, but inside the <Router> */}
-        <Switch>
-          <Route exact path="/">
-            <HelloWidget />
-            <ReservationSection />
-          </Route>
-          <Route path="/registration">
-            <Registration />
-          </Route>
-        </Switch>
-      </>
-    </Router>
+    <EatOutSection />
+    // <Router>
+    //   <>
+    //     {/* A <Switch> looks through its <Route>s and
+    //         renders the first one that matches the path. */}
+    //     {/* If you want to render something everywhere (in each page), render it outside the <Switch>, but inside the <Router> */}
+    //     <Switch>
+    //       <Route exact path="/">
+    //         <HelloWidget />
+    //         <ReservationSection />
+    //       </Route>
+    //       <Route path="/registration">
+    //         <Registration />
+    //       </Route>
+    //     </Switch>
+    //   </>
+    // </Router>
   );
 }
 
