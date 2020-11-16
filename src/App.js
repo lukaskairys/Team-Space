d3ac7aab0dc5df624df7afb3c57d6b3dea9aaa12
aAntak <andriusk1997@gmail.com>
2020-11-16 22:44:03 +0200

    update styles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Registration from "../src/features/registration/components/Registration";
 import HelloWidget from "features/helloWidget/components/HelloWidget";
+import WeatherWidget from "./features/weatherWidget/components/weatherWidget";
 import ReservationSection from "features/reservationsSection/components/ReservationsSection";
 import MainLayout from "components/MainLayout/MainLayout";
 
@@ -18,6 +19,7 @@ function App() {
               <>
                 <HelloWidget />
                 <ReservationSection />
+                <WeatherWidget />
               </>
             </MainLayout>
           </Route>
