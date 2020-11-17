b6fcca2a62612797d52704027735c73419418f86
Martynas <47948087+MartSurv@users.noreply.github.com>
2020-11-18 00:25:35 +0200

    bug fixes

--- src/features/helloWidget/components/HelloWidget.jsx
+++ src/features/helloWidget/components/HelloWidget.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "./helloWidget.scss";
 import jsonserver from "../../../apis/jsonserver";
@@ -10,25 +11,41 @@ const options = {
 const time = new Date().toLocaleTimeString("lt-LT", options);
 
 const HelloWidget = () => {
+  const [mounted, setMounted] = useState(false);
   const [currentTime, setCurrentTime] = useState(time);
   const [userName, setUserName] = useState("Wizard");
 
   useEffect(() => {
-    jsonserver
-      .get("/userData")
-      .then(function ({ data }) {
-        setUserName(data.userName);
-      })
-      .catch(function () {
-        setUserName("Mr. Error");
-      });
+    const CancelToken = axios.CancelToken;
+    const source = CancelToken.source();
+    setMounted(true);
+    const getUserName = async () => {
+      try {
+        const { data } = await jsonserver.get("/userData", {
+          cancelToken: source.token,
+        });
+        setUserName(data.username);
+      } catch (err) {
+        if (err) {
+          setUserName("Mr. Error");
+        }
+      }
+    };
+
+    if (mounted) {
+      getUserName();
+    }
 
     let intervalID = setInterval(() => {
       setCurrentTime(new Date().toLocaleTimeString("lt-LT", options));
     }, 1000);
 
-    return () => clearInterval(intervalID);
-  }, []);
+    return () => {
+      clearInterval(intervalID);
+      source.cancel();
+      setMounted(false);
+    };
+  }, [mounted]);
 
   const renderGreeting = () => {
     const now = parseInt(currentTime.slice(0, 2));
