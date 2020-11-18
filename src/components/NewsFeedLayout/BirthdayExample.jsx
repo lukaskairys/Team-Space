972e23dfbc7ca49bca9a3f24ba92e3b4a24db896
Martynas <47948087+MartSurv@users.noreply.github.com>
2020-11-18 15:53:38 +0200

    Temporary commit

---
+++ src/components/NewsFeedLayout/BirthdayExample.jsx
@@ -0,0 +1,6 @@
+// This component is only for demonstration purposes
+import React from "react";
+
+export default function NewsFeed() {
+  return <div style={{ backgroundColor: "#FFAC81", height: "232px" }}></div>;
+}
---
+++ src/components/NewsFeedLayout/PostExample.jsx
@@ -0,0 +1,6 @@
+// This component is only for demonstration purposes
+import React from "react";
+
+export default function NewsFeed() {
+  return <div style={{ backgroundColor: "#CDEAC0", height: "612px" }}></div>;
+}
