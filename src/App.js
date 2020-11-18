fad43e914e34742a0c1bbc7b4200aeca7b22e61a
Lukas Kairys <lukaskairys@gmail.com>
2020-11-18 14:38:04 +0200

    breadcrumbs completed

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import HelloWidget from "features/helloWidget/components/HelloWidget";
 import WeatherWidget from "./features/weatherWidget/components/weatherWidget";
 import ReservationSection from "features/reservationsSection/components/ReservationsSection";
 import MainLayout from "components/MainLayout/MainLayout";
+import Breadcrumbs from "components/Breadcrumbs/Breadcrumbs";
 
 function App() {
   return (
@@ -26,11 +27,22 @@ function App() {
           </Route>
           <Route exact path="/reservations">
             <MainLayout>
-              <ReservationSection />
+              <>
+                <Breadcrumbs />
+                <ReservationSection />
+              </>
             </MainLayout>
           </Route>
-          <Route exact path="/reservations/devices"></Route>
-          <Route exact path="/reservations/books"></Route>
+          <Route exact path="/reservations/devices">
+            <MainLayout>
+              <Breadcrumbs />
+            </MainLayout>
+          </Route>
+
+          <Route exact path="/reservations/books">
+            <MainLayout />
+          </Route>
+
           <Route path="/registration">
             <RegistrationPage />
           </Route>
--- src/components/Breadcrumbs/Breadcrumbs.jsx
+++ src/components/Breadcrumbs/Breadcrumbs.jsx
@@ -1,11 +1,36 @@
 import React from "react";
-import "./Breadcrumbs.scss";
+import classNames from "classnames";
+import { Link } from "react-router-dom";
+
 import { ReactComponent as Arrow } from "assets/icons/right.svg";
 
+import "./Breadcrumbs.scss";
+
 const Breadcrumbs = () => {
+  const pathnames = window.location.pathname.split("/").filter((x) => x);
   return (
     <nav className="breadcrumbs">
-      <Arrow />
+      <Link className="breadcrumbs__item" to="/">
+        dashboard
+      </Link>
+      {pathnames.map((pathItem, index) => {
+        const routeTo = `/${pathnames.slice(0, index + 1).join("/")}`;
+
+        return (
+          <React.Fragment key={index}>
+            <Arrow className="breadcrumbs__icon" />
+            <Link
+              className={classNames("breadcrumbs__item", {
+                "breadcrumbs__item--last":
+                  pathItem === pathnames[pathnames.length - 1],
+              })}
+              to={routeTo}
+            >
+              {pathItem}
+            </Link>
+          </React.Fragment>
+        );
+      })}
     </nav>
   );
 };
