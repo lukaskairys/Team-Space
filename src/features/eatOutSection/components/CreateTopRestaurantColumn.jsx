17f3799574d727fbf34804c5bbd4235845de1ac4
aAntak <andriusk1997@gmail.com>
2020-11-18 11:42:48 +0200

    add css, import svg as react component

--- src/features/eatOutSection/components/CreateTopRestaurantColumn.jsx
+++ src/features/eatOutSection/components/CreateTopRestaurantColumn.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import RatingComponent from "../../../components/Rating/Rating";
 import PersonComponent from "./PersonCounter";
 import { createWorkingSchedule, isOpen } from "./RestaurantWorkingSchedule";
+import { ReactComponent as HeartIcon } from "assets/icons/heart.svg";
 
 const CreateTopRestaurantColumn = (restaurant) => {
   return (
@@ -21,13 +22,10 @@ const CreateTopRestaurantColumn = (restaurant) => {
       </div>
       <div className="eat-out-card__subsection">
         <h3 className="eat-out-card__title">{formTitle(restaurant.name)}</h3>
-        <svg className="eat-out-card__favorite-container" viewBox="0 0 24 22">
-          <path
-            onClick={toggleFavorite}
-            className="eat-out-card__favorite--active eat-out-card__favorite"
-            d="M20.84 3.60999C20.3292 3.099 19.7228 2.69364 19.0554 2.41708C18.3879 2.14052 17.6725 1.99817 16.95 1.99817C16.2275 1.99817 15.5121 2.14052 14.8446 2.41708C14.1772 2.69364 13.5708 3.099 13.06 3.60999L12 4.66999L10.94 3.60999C9.9083 2.5783 8.50903 1.9987 7.05 1.9987C5.59096 1.9987 4.19169 2.5783 3.16 3.60999C2.1283 4.64169 1.54871 6.04096 1.54871 7.49999C1.54871 8.95903 2.1283 10.3583 3.16 11.39L4.22 12.45L12 20.23L19.78 12.45L20.84 11.39C21.351 10.8792 21.7563 10.2728 22.0329 9.60535C22.3095 8.93789 22.4518 8.22248 22.4518 7.49999C22.4518 6.77751 22.3095 6.0621 22.0329 5.39464C21.7563 4.72718 21.351 4.12075 20.84 3.60999Z"
-          />
-        </svg>
+        <HeartIcon
+          onClick={toggleFavorite}
+          className="eat-out-card__favorite"
+        />
       </div>
       {formTime(restaurant.openingHours)}
     </div>
