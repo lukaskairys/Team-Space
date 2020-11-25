798d0cdb79784a9bbdcb8af0ee1f2c48c1f52a29
Martynas <47948087+MartSurv@users.noreply.github.com>
2020-11-25 18:03:44 +0200

    added comments, sparated logic for laying out big and small cards, moved useWindowDimensions hook to the global scope

--- src/components/NewsFeedLayout/LayoutChildren.jsx
+++ src/components/NewsFeedLayout/LayoutChildren.jsx
@@ -1,6 +1,5 @@
-import React from "react";
-
-import useWindowDimensions from "./useWindowDimensions";
+import useWindowDimensions from "../../utils/useWindowDimensions";
+import rearrangeCards from "./rearrangeCards";
 
 function groupArray(arr, size) {
   let groupedArr = [];
@@ -10,28 +9,10 @@ function groupArray(arr, size) {
   return groupedArr;
 }
 
-const gridRowStyle = {
-  addRows: function (value, n, m) {
-    if (n === false) {
-      return "";
-    } else {
-      return value
-        .split(" ")
-        .map((el) => {
-          const n = Number(el);
-          return isNaN(n) ? el : n;
-        })
-        .map((el) => (typeof el === "number" ? el + 3 * n - m : el))
-        .join(" ");
-    }
-  },
-};
-
 const LayoutChildren = ({ children }) => {
   const { width } = useWindowDimensions();
   let smallCardsTemp = [];
   let bigCardsTemp = [];
-  let counter = 0;
 
   children.forEach((item) => {
     if (item.props.type === 1) {
@@ -41,6 +22,7 @@ const LayoutChildren = ({ children }) => {
     }
   });
 
+  // Take an array of children and group it by screen size
   if (width < 1500 && width > 1080) {
     bigCardsTemp = groupArray(bigCardsTemp, 2);
     smallCardsTemp = groupArray(smallCardsTemp, 2);
@@ -52,97 +34,9 @@ const LayoutChildren = ({ children }) => {
     smallCardsTemp = groupArray(smallCardsTemp, 3);
   }
 
-  const smallCards = smallCardsTemp.map((setOfCards, setOfCardsIndex, arr) => {
-    return setOfCards.map((card, index) => {
-      if (arr[arr.length - 2] !== undefined) {
-        if (
-          (index === 2 && setOfCards.length === 3) ||
-          (index === 1 &&
-            setOfCards.length === 2 &&
-            arr[arr.length - 2].length !== 3) ||
-          (index === 0 &&
-            setOfCards.length === 1 &&
-            arr[arr.length - 2].length !== 2)
-        )
-          return (
-            <div
-              key={index}
-              style={{
-                marginTop: "3rem",
-              }}
-            >
-              {card}
-            </div>
-          );
-        else return card;
-      } else {
-        return card;
-      }
-    });
-  });
+  const cards = rearrangeCards(bigCardsTemp, smallCardsTemp, width);
 
-  const bigCards = bigCardsTemp.map((setOfCards, setOfCardsIndex) => {
-    if (smallCards[setOfCardsIndex] === undefined) counter++;
-    return setOfCards.map((card, index, setOfCardsArr) => {
-      switch (index) {
-        case 0:
-          return (
-            <div
-              key={index}
-              style={{
-                gridRow: gridRowStyle.addRows(
-                  "2 / 4",
-                  setOfCardsIndex,
-                  counter
-                ),
-              }}
-            >
-              {card}
-            </div>
-          );
-        case 1:
-          return (
-            <div
-              key={index}
-              style={{
-                gridColumn:
-                  setOfCardsArr.length === 3 || setOfCardsArr.length === 2
-                    ? "2 / 3"
-                    : "",
-                gridRow: gridRowStyle.addRows(
-                  "1 / 3",
-                  setOfCardsArr.length === 3 || setOfCardsArr.length === 2
-                    ? setOfCardsIndex
-                    : false,
-                  counter
-                ),
-              }}
-            >
-              {card}
-            </div>
-          );
-        case 2:
-          return (
-            <div
-              key={index}
-              style={{
-                gridColumn: setOfCards.length === 3 ? "3 / 4" : "",
-                gridRow: gridRowStyle.addRows(
-                  "2 / 4",
-                  setOfCardsArr.length === 3 ? setOfCardsIndex : false,
-                  counter
-                ),
-              }}
-            >
-              {card}
-            </div>
-          );
-        default:
-          return card;
-      }
-    });
-  });
-  return [...smallCards, ...bigCards];
+  return [cards];
 };
 
 export default LayoutChildren;
---
+++ src/components/NewsFeedLayout/Placeholder.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Placeholder() {
+  return (
+    <div className="news-feed-section__placeholder">
+      <span>...</span>
+    </div>
+  );
+}
