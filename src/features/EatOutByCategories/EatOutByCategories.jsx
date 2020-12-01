6da3aec13fdc8ef08c68c63e47df965a5aa3cb41
Kristina Stepanaviciene <kr.stepanaviciene@gmail.com>
2020-12-01 10:00:34 +0200

    errors handling

--- src/features/EatOutByCategories/EatOutByCategories.jsx
+++ src/features/EatOutByCategories/EatOutByCategories.jsx
@@ -7,7 +7,7 @@ import "./eatOutByCategory.scss";
 
 function EatOutCategoriesPage() {
   const { category } = useParams();
-  const { data } = useContext(context);
+  const { data, error, isLoading } = useContext(context);
 
   const getRestaurants = () => {
     if (Object.keys(data).length !== 0) {
@@ -19,7 +19,6 @@ function EatOutCategoriesPage() {
       return restaurants;
     }
   };
-
   const restaurants = getRestaurants();
 
   return (
@@ -31,6 +30,13 @@ function EatOutCategoriesPage() {
         </span>
       </h1>
       <section className="categories-page__section">
+        {isLoading && <span></span>}
+        {error && <span>Sorry, failed to fetch data.</span>}
+        {restaurants !== undefined && restaurants.length === 0 ? (
+          <span>This category has no restaurants in our database.</span>
+        ) : (
+          ""
+        )}
         {restaurants &&
           restaurants.map((restaurant) => (
             <EatOutCard id={restaurant.id} key={restaurant.id} />
