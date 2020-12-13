f6dfc0f915fbf1355cbe7f0e78dfef8ba76bc765
aAntak <andriusk1997@gmail.com>
2020-12-14 01:39:27 +0200

    Update EatOutSection.jsx

--- src/features/eatOutSection/components/EatOutSection.jsx
+++ src/features/eatOutSection/components/EatOutSection.jsx
@@ -10,7 +10,7 @@ const EatOutSection = () => {
   const count = 2;
   const { restaurants } = FetchBestRatedRestaurants(count);
 
-  //Generated this mockdata method i receive constant count of restaurants and dont get an following error - "Error: Rendered more hooks than during the previous render."
+  //Generated this mockdata method  so i receive constant count of restaurants and dont get an following error - "Error: Rendered more hooks than during the previous render."
   //(I map restaurants which then calls hooks used for check in.). There might be a better solution
   const mockData = () => {
     const data = [];
