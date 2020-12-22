cf200a0ef8dc22817b170e201405ed29b05c2715
aAntak <andriusk1997@gmail.com>
2020-12-22 19:05:56 +0200

    fix errors, update functionality

---
+++ src/features/weatherWidget/components/utils/deconstructAPI.js
@@ -0,0 +1,25 @@
+import { isObjectEmpty } from "../../../../utils/objects";
+export const deconstructAPI = (
+  data,
+  setError,
+  setItems,
+  setLoading,
+  isMounted
+) => {
+  if (isObjectEmpty(data)) {
+    setError("Unable to retrieve data");
+    return;
+  }
+  if (isMounted.current) {
+    setItems({
+      temp: data.main.temp.toFixed(),
+      humidity: data.main.humidity,
+      windSpeed: data.wind.speed,
+      description: data.weather[0].description,
+      location: data.name,
+      conditionId: data.weather[0].id,
+      clouds: data.clouds.all,
+    });
+    setLoading(true);
+  }
+};
