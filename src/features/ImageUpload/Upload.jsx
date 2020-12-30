7cb68481b25c7ff625e6acbce7b3f420e09d6b43
Martynas <47948087+MartSurv@users.noreply.github.com>
2020-12-30 15:49:00 +0200

    pretty much done

--- src/features/ImageUpload/Upload.jsx
+++ src/features/ImageUpload/Upload.jsx
@@ -1,10 +1,17 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, {
+  useState,
+  useEffect,
+  useContext,
+  useCallback,
+  useRef,
+} from "react";
 import Cropper from "react-easy-crop";
-import axios from "axios";
 import { FilePond, registerPlugin } from "react-filepond";
 import FilePondPluginImagePreview from "filepond-plugin-image-preview";
 import FilePondPluginImageCrop from "filepond-plugin-image-crop";
+import FilePondPluginImageResize from "filepond-plugin-image-resize";
 import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
+import FilePondPluginImageTransform from "filepond-plugin-image-transform";
 import FilePondPluginFileEncode from "filepond-plugin-file-encode";
 import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
 import FilePondPluginFileValidateSize from "filepond-plugin-file-validate-size";
@@ -12,16 +19,21 @@ import FilePondPluginFileValidateSize from "filepond-plugin-file-validate-size";
 import "filepond/dist/filepond.min.css";
 import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
 
-import { patch } from "apis/services";
 import { UserContext } from "contexts/UserContext";
+import Button from "components/button/Button";
+import { warnToast } from "components/Toasts/ToastHandler";
 
 import "./upload.scss";
-import b64toBlob from "./b64toBlob";
+import b64toBlob from "./helpers/b64toBlob";
+import serverConfig from "./helpers/serverConfig";
+import getCroppedImg from "./helpers/cropImage";
 
 registerPlugin(
   FilePondPluginImagePreview,
   FilePondPluginImageCrop,
+  FilePondPluginImageResize,
   FilePondPluginImageExifOrientation,
+  FilePondPluginImageTransform,
   FilePondPluginFileEncode,
   FilePondPluginFileValidateType,
   FilePondPluginFileValidateSize
@@ -29,10 +41,14 @@ registerPlugin(
 
 function Upload() {
   const [files, setFiles] = useState([]);
+  const [imageB64, setImageB64] = useState(null);
   const [imageAdded, setImageAdded] = useState(false);
   const { data: user } = useContext(UserContext);
   const [crop, setCrop] = useState({ x: 0, y: 0 });
   const [zoom, setZoom] = useState(1);
+  const [croppedAreaPixels, setCroppedAreaPixels] = useState(null);
+  const pondRef = useRef(null);
+  const config = serverConfig(user, files);
 
   useEffect(() => {
     if (user.userImage) {
@@ -45,72 +61,45 @@ function Upload() {
     }
   }, [user.userImage]);
 
-  const serverConfig = {
-    process: (
-      fieldName,
-      file,
-      metadata,
-      load,
-      error,
-      progress,
-      abort,
-      transfer,
-      options
-    ) => {
-      const CancelToken = axios.CancelToken;
-      const source = CancelToken.source();
+  useEffect(() => {
+    if (files.length > 0 && files[0].getFileEncodeBase64String?.())
+      setImageB64(files[0].getFileEncodeBase64String());
+  }, [files, imageB64]);
 
-      patch(
-        "users",
-        {
-          userImage: files[0].getFileEncodeBase64String(),
-        },
-        user.id,
-        {
-          cancelToken: source.token,
-          headers: { "Content-Type": "application/json" },
-          onUploadProgress: (e) => {
-            progress(e.lengthComputable, e.loaded, e.total);
-          },
-        }
-      )
-        .then((res) => {
-          if (res.status >= 200 && res.status < 300) {
-            load();
-          }
-        })
-        .catch((thrown) => {
-          if (axios.isCancel(thrown)) {
-            error(thrown.message);
-          } else {
-            error("Failed to upload the image");
-          }
-        });
-      return {
-        abort: () => {
-          source.cancel("Operation canceled by the user.");
-        },
-      };
-    },
-    revert: null,
-    load: null,
-    fetch: null,
-    restore: null,
-    remove: null,
+  const onCropComplete = useCallback((croppedAreaPixels) => {
+    setCroppedAreaPixels(croppedAreaPixels);
+  }, []);
+
+  const addCroppedImage = useCallback(() => {
+    try {
+      const croppedImage = getCroppedImg(imageB64, croppedAreaPixels);
+      pondRef.current.addFile(croppedImage);
+    } catch (e) {
+      warnToast("Failed to add the image!");
+    }
+  }, [croppedAreaPixels, imageB64]);
+
+  const handleButtonConfirm = () => {
+    setImageAdded(false);
+    addCroppedImage();
+  };
+
+  const handleButtonCancel = () => {
+    setImageAdded(false);
   };
 
   if (user) {
     return (
       <>
         <FilePond
+          ref={pondRef}
           files={files}
           onupdatefiles={setFiles}
           onactivatefile={() => setImageAdded(true)}
-          server={serverConfig}
+          server={config}
           name="userImage"
-          labelIdle='Drag &amp; Drop your image or <span class="filepond--label-action">Browse</span><br>max 100KB'
+          labelIdle='<br>Drag &amp; Drop your image or <span class="filepond--label-action">Browse</span><br><br>max 150KB'
           imageCropAspectRatio="1:1"
-          stylePanelAspectRatio="1:1"
           stylePanelLayout="compact circle"
           styleButtonRemoveItemPosition="bottom center"
           styleButtonProcessItemPosition="bottom left"
@@ -118,20 +107,23 @@ function Upload() {
           styleProgressIndicatorPosition="bottom left"
           credits={false}
           acceptedFileTypes={["image/jpeg"]}
-          maxFileSize="100KB"
+          maxFileSize="150KB"
           instantUpload={false}
         />
         {imageAdded && (
           <div className="crop-container">
             <Cropper
-              image={`data:image/jpeg;base64,${files[0].getFileEncodeBase64String()}`}
+              image={`data:image/jpeg;base64,${imageB64}`}
               cropShape={"round"}
               crop={crop}
               zoom={zoom}
               aspect={1 / 1}
               onCropChange={setCrop}
               onZoomChange={setZoom}
+              onCropComplete={onCropComplete}
             />
+            <Button handleClick={handleButtonConfirm}>Confirm crop</Button>
+            <Button handleClick={handleButtonCancel}>Cancel</Button>
           </div>
         )}
       </>
