e902dec5d5e81d307cd97bf9eb52a198f6abaa78
aAntak <andriusk1997@gmail.com>
2021-01-12 13:30:08 +0200

    update heart icon

--- src/components/HeartIcon/HeartIcon.jsx
+++ src/components/HeartIcon/HeartIcon.jsx
@@ -1,13 +1,13 @@
-import React, { useState, useEffect, useCallback, useContext } from "react";
+import React from "react";
 import classNames from "classnames";
+
 import PropTypes from "prop-types";
 
-import { FavoriteTypes } from "../../utils/FavoriteTypes";
-import { isObjectEmpty } from "../../utils/objects";
 import Button from "components/button/Button";
 import { ReactComponent as Heart } from "assets/icons/heart.svg";
-import { UserContext } from "../../contexts/UserContext";
-import { patch } from "../../apis/services";
+import useHeartIcon from "./useHeartIcon";
+import { FavoriteTypes } from "../../utils/FavoriteTypes";
+import "./heartIcon.scss";
 
 function HeartIcon({
   clickEvent,
@@ -17,92 +17,24 @@ function HeartIcon({
   title,
   feedCard,
 }) {
-  const [active, setActive] = useState(false);
-
-  const { data, likeState, setLikeState } = useContext(UserContext);
-
-  const heartClass = classNames({
-    "heart-icon": true,
-    "heart-icon--active": active,
-    "heart-icon--news": strokeColor === "slate-gray",
-    "heart-icon--feed-card": feedCard,
-  });
-
-  const toggleFavorite = () => {
-    active === true ? remove(itemId, data) : add(itemId, data);
-    setActive(!active);
-    setLikeState(`${itemId}-${active}`);
-    if (clickEvent !== undefined) {
-      clickEvent(active);
-    }
-  };
-
-  const genericHandler = useCallback(
-    (userInfo, itemId, handler) => {
-      switch (itemType) {
-        case FavoriteTypes.BOOK:
-          handler(userInfo.liked.books, itemId);
-          break;
-        case FavoriteTypes.STORY:
-          handler(userInfo.liked.stories, itemId);
-          break;
-        case FavoriteTypes.RESTAURANT:
-          handler(userInfo.liked.restaurants, itemId);
-          break;
-        case FavoriteTypes.DEVICE:
-          handler(userInfo.liked.devices, itemId);
-          break;
-        case FavoriteTypes.ROOM:
-          handler(userInfo.liked.rooms, itemId);
-          break;
-        default:
-          break;
-      }
-    },
-    [itemType]
+  const { active, toggleFavorite } = useHeartIcon(
+    itemId,
+    itemType,
+    clickEvent,
+    FavoriteTypes
   );
-
-  const remove = (itemId, data) => {
-    genericHandler(data, itemId, removeHandler);
-    patch("/users", data, data.id);
-  };
-
-  const add = (itemId, user) => {
-    genericHandler(user, itemId, addHandler);
-    patch("/users", user, user.id);
-  };
-
-  useEffect(() => {
-    if (!isObjectEmpty(data)) {
-      genericHandler(data, itemId, InitialHandler);
-    }
-  }, [data, genericHandler, itemId, likeState]);
-
-  const removeHandler = (user, id) => {
-    if (!user) return;
-    const removeIndex = user.map((like) => like.id).indexOf(id);
-
-    ~removeIndex && user.splice(removeIndex, 1);
-  };
-
-  const addHandler = (user, id) => {
-    if (!user) return;
-    if (!user.some((e) => e.id === id)) {
-      user.push({ id });
-    }
-  };
-
-  const InitialHandler = (user, id) => {
-    if (!user) return;
-    const doesContain = user.some((liked) => liked.id === id);
-    if (doesContain) setActive(true);
-    else setActive(false);
-  };
+  const heartClass = classNames({
+    "favorite__heart-icon": true,
+    "favorite__heart-icon--active": active,
+    "favorite__heart-icon--news": strokeColor === "slate-gray",
+    "favorite__heart-icon--feed-card": feedCard,
+  });
 
   return (
     <Button
       handleClick={toggleFavorite}
       empty={true}
+      concreteClass={"favorite"}
       ariaLabel={
         !active
           ? `Add ${title} ${itemType} to favorite`
